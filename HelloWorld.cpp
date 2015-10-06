bfdb22bb6b9ad526fa34955987d35488113d856a
CorrieASmith <corriesmith87@gmail.com>
2015-10-06 16:08:12 -0700

    practice with output statements

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
+#include <iostream> //preprocessor directive that tells compiler to add contents of iostream header to program
 
-int main()
+int main() //declares main function
 {
-  std::cout << "Hello world!" << std::endl;
-  return 0;
+  std::cout << "Hello world!" << std::endl; //output statement with << output operator
+  std::cout << "Test" << std::endl;
+  std::cout << "Goodbye" << std::endl;
+  return 0; //return statement that determines whether value was successful or not
 }
+
+//int x; <-- declaration statement (declares a variable)
+//x = 5; <-- assignment statement (assigns a value to the variable)
+//std::cout << x; <-- output statement  (outputs the value of x)
